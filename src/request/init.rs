5531524dd04e5fa9d19923b06e05a7e728bcaed1
Thomas Newman <tnewman@users.noreply.github.com>
2020-12-24 13:45:08 -0500

    Implemented Init request parsing

--- src/request/init.rs
+++ src/request/init.rs
@@ -1,6 +1,7 @@
-use crate::error::Error;
-use bytes::Buf;
 use std::convert::TryFrom;
+
+use crate::error::Error;
+use crate::try_buf::TryBuf;
 
 #[derive(Debug, PartialEq)]
 pub struct Init {
@@ -13,12 +14,31 @@ impl TryFrom<&[u8]> for Init {
     fn try_from(item: &[u8]) -> Result<Self, Self::Error> {
         let mut bytes = item;
 
-        if bytes.remaining() < 1 {
-            return Err(Error::BadMessage);
-        }
-
         Ok(Init {
-            version: bytes.get_u8(),
+            version: bytes.try_get_u8()?,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use bytes::Buf;
+    use bytes::BufMut;
+
+    #[test]
+    fn test_parse_init_message() {
+        let mut init_bytes = vec![];
+        init_bytes.put_u8(0x03);
+
+        assert_eq!(Init::try_from(&init_bytes[..]), Ok(Init { version: 0x03 }));
+    }
+
+    #[test]
+    fn test_parse_invalid_message() {
+        let init_bytes: &[u8] = &[];
+
+        assert_eq!(Init::try_from(init_bytes), Err(Error::BadMessage));
     }
 }
