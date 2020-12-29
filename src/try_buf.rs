2649542b132272390e6527f6ee7273037060175e
Thomas Newman <tnewman@users.noreply.github.com>
2020-12-29 08:57:51 -0500

    Added tests to TryBuf

--- src/try_buf.rs
+++ src/try_buf.rs
@@ -66,3 +66,93 @@ impl<T: Buf> TryBuf for T {
         Ok(string)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_try_get_u8() {
+        let u8_bytes: Vec<u8> = vec![0x01, 0x02];
+
+        assert_eq!(u8_bytes.as_slice().try_get_u8(), Ok(0x01));
+    }
+
+    #[test]
+    fn test_try_get_u8_with_invalid_data() {
+        assert_eq!(vec![].as_slice().try_get_u8(), Err(Error::BadMessage));
+    }
+
+    #[test]
+    fn test_try_get_u32() {
+        let u32_bytes: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01];
+
+        assert_eq!(u32_bytes.as_slice().try_get_u32(), Ok(0x01));
+    }
+
+    #[test]
+    fn test_try_get_u32_with_invalid_data() {
+        let u32_bytes: Vec<u8> = vec![0x00, 0x00, 0x01];
+
+        assert_eq!(u32_bytes.as_slice().try_get_u32(), Err(Error::BadMessage));
+    }
+
+    #[test]
+    fn test_try_get_u64() {
+        let u64_bytes: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01];
+
+        assert_eq!(u64_bytes.as_slice().try_get_u64(), Ok(0x01));
+    }
+
+    #[test]
+    fn test_try_get_u64_with_invalid_data() {
+        let u64_bytes: Vec<u8> = vec![0x00];
+
+        assert_eq!(u64_bytes.as_slice().try_get_u64(), Err(Error::BadMessage));
+    }
+
+    #[test]
+    fn test_try_get_bytes() {
+        let bytes: Vec<u8> = vec![0x00, 0x01];
+
+        assert_eq!(
+            bytes.as_slice().try_get_bytes(1),
+            Ok(Bytes::from(vec![0x00]))
+        );
+    }
+
+    #[test]
+    fn test_try_get_bytes_with_overrun() {
+        let bytes: Vec<u8> = vec![0x00];
+
+        assert_eq!(bytes.as_slice().try_get_bytes(2), Err(Error::BadMessage));
+    }
+
+    #[test]
+    fn test_try_get_string() {
+        let string: Vec<u8> = vec![0x00, 0x00, 0x00, 0x04, 0x54, 0x45, 0x53, 0x54]; // TEST
+
+        assert_eq!(string.as_slice().try_get_string(), Ok(String::from("TEST")))
+    }
+
+    #[test]
+    fn test_try_get_string_with_invalid_length() {
+        let string: Vec<u8> = vec![0x00, 0x01]; // length must be 4 bytes (uint32)
+
+        assert_eq!(string.as_slice().try_get_string(), Err(Error::BadMessage))
+    }
+
+    #[test]
+    fn test_try_get_string_with_mismatched_length() {
+        let string: Vec<u8> = vec![0x00, 0x00, 0x00, 0x08, 0x54, 0x45, 0x53, 0x54]; // TEST with length 8
+
+        assert_eq!(string.as_slice().try_get_string(), Err(Error::BadMessage))
+    }
+
+    #[test]
+    fn test_try_get_string_with_invalid_utf8() {
+        let string: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01, 0xFF];
+
+        assert_eq!(string.as_slice().try_get_string(), Err(Error::BadMessage))
+    }
+}
