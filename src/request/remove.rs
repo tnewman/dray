2e5fce6cb7dd639d2d8e32c6bdbcefac3e0a7b73
Thomas Newman <tnewman@users.noreply.github.com>
2020-12-29 09:12:24 -0500

    Added Remove message

--- src/request/remove.rs
+++ src/request/remove.rs
@@ -1,13 +1,74 @@
 use crate::error::Error;
+use crate::try_buf::TryBuf;
 use std::convert::TryFrom;
 
 #[derive(Debug, PartialEq)]
-pub struct Remove {}
+pub struct Remove {
+    id: u32,
+    filename: String,
+}
 
 impl TryFrom<&[u8]> for Remove {
     type Error = Error;
 
-    fn try_from(_item: &[u8]) -> Result<Self, Self::Error> {
-        Err(Error::Unimplemented)
+    fn try_from(item: &[u8]) -> Result<Self, Self::Error> {
+        let mut read_bytes = item;
+
+        let id = read_bytes.try_get_u32()?;
+        let filename = read_bytes.try_get_string()?;
+
+        Ok(Remove { id, filename })
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    use bytes::BufMut;
+    use std::{convert::TryInto, vec};
+
+    #[test]
+    fn test_parse_remove() {
+        let mut read_bytes = vec![];
+
+        read_bytes.put_u32(0x01); // id
+        let filename = "/filename".as_bytes();
+        read_bytes.put_u32(filename.len().try_into().unwrap()); // filename length
+        read_bytes.put_slice(filename); // filename
+
+        assert_eq!(
+            Remove::try_from(read_bytes.as_slice()),
+            Ok(Remove {
+                id: 0x01,
+                filename: String::from("/filename")
+            })
+        );
+    }
+
+    #[test]
+    fn test_parse_remove_with_invalid_id() {
+        let mut read_bytes = vec![];
+
+        read_bytes.put_u8(0x01); // invalid id
+
+        assert_eq!(
+            Remove::try_from(read_bytes.as_slice()),
+            Err(Error::BadMessage)
+        );
+    }
+
+    #[test]
+    fn test_parse_remove_with_invalid_filename() {
+        let mut read_bytes = vec![];
+
+        read_bytes.put_u32(0x01); // id
+        read_bytes.put_u32(0x10); // bad length
+
+        assert_eq!(
+            Remove::try_from(read_bytes.as_slice()),
+            Err(Error::BadMessage)
+        );
     }
 }
