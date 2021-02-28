b99c0550da2860782ba91a103c32fd86f3fe5855
Thomas Newman <tnewman@users.noreply.github.com>
2021-02-27 23:04:23 -0500

    Added thrussh boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,60 @@ pub mod error;
 pub mod protocol;
 pub mod try_buf;
 
-pub fn run_server() {
-    println!("Hello world!");
+use anyhow::Error;
+use futures::future::{Ready, ready};
+use std::{sync::Arc, time::Duration};
+use thrussh_keys::key::KeyPair;
+use thrussh::server::{Auth, Config, Handler, run, Session, Server};
+
+#[tokio::main]
+pub async fn run_server() {
+    let mut config = Config::default();
+    config.connection_timeout = Some(Duration::from_secs(3));
+    config.auth_rejection_time = Duration::from_secs(3);
+    config.keys.push(KeyPair::generate_ed25519().unwrap());
+
+    let config = Arc::new(config);
+
+    let dray_ssh_server = DraySshServer::new();
+
+    run(config, "0.0.0.0:2222", dray_ssh_server).await.unwrap()
 }
+
+#[derive(Clone)]
+struct DraySshServer {
+
+}
+
+impl DraySshServer {
+    pub fn new() -> DraySshServer {
+        DraySshServer {}
+    }
+}
+
+impl Server for DraySshServer {
+    type Handler = Self;
+
+    fn new(&mut self, _peer_addr: Option<std::net::SocketAddr>) -> Self::Handler {
+        self.clone()
+    }
+}
+
+impl Handler for DraySshServer {
+    type Error = Error;
+    type FutureAuth = Ready<Result<(Self, Auth), anyhow::Error>>;
+    type FutureBool = Ready<Result<(Self, Session, bool), anyhow::Error>>;
+    type FutureUnit = Ready<Result<(Self, Session), anyhow::Error>>;
+
+    fn finished_auth(self, auth: Auth) -> Self::FutureAuth {
+        ready(Ok((self, auth)))
+    }
+
+    fn finished_bool(self, b: bool, session: Session) -> Self::FutureBool {
+        ready(Ok((self, session, b)))
+    }
+
+    fn finished(self, session: Session) -> Self::FutureUnit {
+        ready(Ok((self, session)))
+    }
+} 
