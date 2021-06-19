cc4432f82bfbfcd1f656574f6797a0201838b4b4
Thomas Newman <tnewman@users.noreply.github.com>
2021-06-19 14:46:51 -0400

    Do not log entire byte arrays on debug

--- src/protocol/response/data.rs
+++ src/protocol/response/data.rs
@@ -1,8 +1,9 @@
 use bytes::{BufMut, Bytes, BytesMut};
 use std::convert::From;
 use std::convert::TryInto;
+use std::fmt::Debug;
 
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub struct Data {
     pub id: u32,
     pub data: Vec<u8>,
@@ -17,6 +18,15 @@ impl From<&Data> for Bytes {
         data_bytes.put_slice(&data.data);
 
         data_bytes.freeze()
+    }
+}
+
+impl Debug for Data {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+         f.debug_struct("Data")
+          .field("id", &self.id)
+          .field("len", &self.data.len())
+          .finish()
     }
 }
 
@@ -39,5 +49,15 @@ mod test {
         assert_eq!(0x02, data_bytes.get_u32());
         assert_eq!(0x02, data_bytes.get_u8());
         assert_eq!(0x03, data_bytes.get_u8());
+    }
+
+    #[test]
+    fn test_debug_formats_efficient_debug_string() {
+        let data = Data {
+            id: 0x01,
+            data: vec![0x02, 0x03],
+        };
+
+        assert_eq!("Data { id: 1, len: 2 }", format!("{:?}", data));
     }
 }
