4a1c895981ecad7a757c8f8ee0e39a52628b776d
Thomas Newman <tnewman@users.noreply.github.com>
2021-09-13 23:35:45 -0400

    Added sftp client to integration test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod config;
-mod error;
+pub mod error;
 mod protocol;
 mod sftp_session;
 mod ssh_keys;
@@ -195,6 +195,13 @@ impl Handler for DraySshServer {
                 Box::pin(ready(Ok((self, session))))
             }
         }
+    }
+
+    fn channel_eof(self, channel: ChannelId, mut session: Session) -> Self::FutureUnit {
+        // Certain clients, such as sftp, will hold open the session after sending EOF until
+        // the server closes the session.
+        session.close(channel);
+        Box::pin(ready(Ok((self, session))))
     }
 
     fn finished_bool(self, b: bool, session: Session) -> Self::FutureBool {
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,45 +1,89 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::{env, process::Stdio};
+
+use async_once::AsyncOnce;
 use dray::{
-    config::DrayConfig, storage::s3::S3StorageFactory, storage::StorageFactory, DraySshServer,
+    config::DrayConfig, error::Error, storage::s3::S3StorageFactory, storage::StorageFactory,
+    DraySshServer,
 };
 use log::LevelFilter;
-use std::{env, net::TcpStream, thread::sleep, time::Duration};
-use tokio::runtime::Runtime;
 
-fn setup() {
-    env_logger::Builder::new()
-        .filter_level(LevelFilter::Info)
-        .init();
+use tokio::{
+    io::AsyncWriteExt,
+    net::TcpStream,
+    process::Command,
+    spawn,
+    time::{sleep, Duration},
+};
 
-    env::set_var("DRAY_HOST", "127.0.0.1:2222");
-    env::set_var("DRAY_SSH_KEY_PATHS", ".ssh/id_ed25519");
-    env::set_var("DRAY_S3_BUCKET", "integration-test");
-    env::set_var("DRAY_S3_ENDPOINT_NAME", "http://localhost:9000");
-    env::set_var("DRAY_S3_ENDPOINT_REGION", "custom");
-    env::set_var("AWS_ACCESS_KEY_ID", "miniouser");
-    env::set_var("AWS_SECRET_ACCESS_KEY", "miniopass");
+struct TestClient {}
 
-    let dray_config = DrayConfig::new().unwrap();
+lazy_static! {
+    static ref TEST_CLIENT: AsyncOnce<TestClient> = AsyncOnce::new(async {
+        env_logger::Builder::new()
+            .filter_level(LevelFilter::Info)
+            .init();
 
-    let s3_storage_factory = S3StorageFactory::new(&dray_config.s3);
-    let s3_storage = s3_storage_factory.create_storage();
+        env::set_var("DRAY_HOST", "127.0.0.1:2222");
+        env::set_var("DRAY_SSH_KEY_PATHS", ".ssh/id_ed25519");
+        env::set_var("DRAY_S3_BUCKET", "integration-test");
+        env::set_var("DRAY_S3_ENDPOINT_NAME", "http://localhost:9000");
+        env::set_var("DRAY_S3_ENDPOINT_REGION", "custom");
+        env::set_var("AWS_ACCESS_KEY_ID", "miniouser");
+        env::set_var("AWS_SECRET_ACCESS_KEY", "miniopass");
 
-    let dray_server = DraySshServer::new(DrayConfig::new().unwrap());
+        let dray_config = DrayConfig::new().unwrap();
 
-    let runtime = Runtime::new().unwrap();
+        let s3_storage_factory = S3StorageFactory::new(&dray_config.s3);
+        let s3_storage = s3_storage_factory.create_storage();
 
-    runtime.block_on(s3_storage.init()).unwrap();
-    runtime.block_on(dray_server.health_check()).unwrap();
-    runtime.spawn(dray_server.run_server());
+        let dray_server = DraySshServer::new(DrayConfig::new().unwrap());
 
-    wait_for_server_listening(&dray_config);
+        s3_storage.init().await.unwrap();
+        dray_server.health_check().await.unwrap();
+        spawn(dray_server.run_server());
+
+        wait_for_server_listening(&dray_config).await;
+
+        TestClient {}
+    });
 }
 
-fn wait_for_server_listening(dray_config: &DrayConfig) {
+async fn execute_sftp_command(command: &str) -> Result<String, Error> {
+    let mut child = Command::new("sftp")
+        .arg("-b-")
+        .arg("-i/home/tnewman/repos/dray/.ssh/id_ed25519")
+        .arg("-oStrictHostKeyChecking=no")
+        .arg("-P2222")
+        .arg("test@127.0.0.1")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let child_stdin = child.stdin.as_mut().unwrap();
+    child_stdin.write_all(command.as_bytes()).await.unwrap();
+    child_stdin.write_all(b"\nquit\n").await.unwrap();
+
+    let output = child.wait_with_output().await.unwrap();
+
+    match output.status.success() {
+        true => Ok(String::from_utf8_lossy(&output.stdout).to_string()),
+        false => Err(Error::Failure(
+            String::from_utf8_lossy(&output.stderr).to_string(),
+        )),
+    }
+}
+
+async fn wait_for_server_listening(dray_config: &DrayConfig) {
     for count in 1..=1000 {
-        match TcpStream::connect(&dray_config.host) {
+        match TcpStream::connect(&dray_config.host).await {
             Ok(_) => break,
-            Err(_) => sleep(Duration::from_millis(10)),
-        }
+            Err(_) => sleep(Duration::from_millis(10)).await,
+        };
 
         if count == 1000 {
             panic!("Could not connect to Dray server. Check the log for startup errors.");
@@ -47,7 +91,12 @@ fn wait_for_server_listening(dray_config: &DrayConfig) {
     }
 }
 
-#[test]
-fn test_list_directory() {
-    setup();
+#[tokio::test]
+async fn test_list_directory() {
+    TEST_CLIENT.get().await;
+
+    execute_sftp_command("ls").await.unwrap();
 }
+
+#[test]
+fn test_upload_directory() {}
